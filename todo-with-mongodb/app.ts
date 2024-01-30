9dde87ee13849d5ba885b3aad7ab1208363779ab
y-l-mochizuki <mczk9402@gmail.com>
2024-01-30 23:16:54 +0900

    add: crud method

--- todo-with-mongodb/app.ts
+++ todo-with-mongodb/app.ts
@@ -7,4 +7,20 @@ app.get("/api/v1/tasks", (req, res) => {
   res.send("タスクを全て取得しました");
 });
 
+app.post("/api/v1/tasks", (req, res) => {
+  res.send("タスクを新規作成しました");
+});
+
+app.get("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを取得しました");
+});
+
+app.patch("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを更新しました");
+});
+
+app.delete("/api/v1/tasks/:id", (req, res) => {
+  res.send("ある特定のタスクを削除しました");
+});
+
 app.listen(PORT, () => console.log("サーバーが起動しました"));
