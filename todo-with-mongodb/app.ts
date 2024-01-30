9587d709288d0366f2dfa767db272b915aa13988
y-l-mochizuki <mczk9402@gmail.com>
2024-01-30 23:07:12 +0900

    feat: get method

--- todo-with-mongodb/app.ts
+++ todo-with-mongodb/app.ts
@@ -2,4 +2,9 @@ import express from "express";
 const app = express();
 const PORT = 5001;
 
+// version1 アプデでエンドポイント変わってもいいようにバージョンをつける
+app.get("/api/v1/tasks", (req, res) => {
+  res.send("タスクを全て取得しました");
+});
+
 app.listen(PORT, () => console.log("サーバーが起動しました"));
