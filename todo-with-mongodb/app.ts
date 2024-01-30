33f151d51fae4b2480204a4c3a2271c87d96c8cf
y-l-mochizuki <mczk9402@gmail.com>
2024-01-31 02:33:30 +0900

    add: mongodb env

--- todo-with-mongodb/app.ts
+++ todo-with-mongodb/app.ts
@@ -1,10 +1,23 @@
+require("dotenv").config();
 import express from "express";
 import { TaskRoute } from "./routes/tasks";
+import { connectDB } from "./db/connect";
 
 const app = express();
 const PORT = 5001;
 
-// ルーティング設定
-app.use("/api/v1/tasks", TaskRoute);
+// データベース接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL || "");
+
+    // ルーティング設定
+    app.use("/api/v1/tasks", TaskRoute);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
 
 app.listen(PORT, () => console.log("サーバーが起動しました"));
